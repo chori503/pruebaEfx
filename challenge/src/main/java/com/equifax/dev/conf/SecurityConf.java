fd552304a6efed7fe897e6a922ac51cec181332c
chori503 <45953869+chori503@users.noreply.github.com>
2020-10-15 12:50:21 -0600

    Agregado seguridad (clase SecurityConf) spring security

--- challenge/src/main/java/com/equifax/dev/conf/SecurityConf.java
+++ challenge/src/main/java/com/equifax/dev/conf/SecurityConf.java
@@ -1,4 +1,14 @@
 package com.equifax.dev.conf;
 
-public class SecurityConf {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+	security.authorizeRequests().antMatchers("/index").permitAll().antMatchers("/").permitAll().anyRequest().authenticated();
+	}
 }
