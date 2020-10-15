748f3018b0e7bcb6ec9d1b4fa80becb8ecd8b89a
chori503 <45953869+chori503@users.noreply.github.com>
2020-10-15 12:52:54 -0600

    creando controlador para mostrar el index.html y agregando texto informativo en el mismo

---
+++ challenge/src/main/java/com/equifax/dev/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.equifax.dev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public String index(){
+		return "index";
+	}
+	@GetMapping("/index")
+	public String index2() {
+		return "index";
+	}
+}
